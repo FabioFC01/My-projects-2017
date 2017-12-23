9b91efb00d920e1942fd31876211a1fca65d0b0a
Fabio Cociancich <fcociancich@gmail.com>
2017-12-23 18:58:30 +0100

    Add files via upload

---
+++ 00000000000000000.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#define square(X) ((X)*(X))
+
+int main() {
+	double a, b;
+	double c = 13;
+
+	a = square(5);			// 25
+	b = square(4.5 + 7.5);	// 12*12 = 144
+
+	cout << "a   = " << a << endl;
+	cout << "b   = " << b << endl;
+	cout << "c   = " << c;
+	cout << " , c squared = " << square(c++) << endl;
+	cout << "Now c = " << c << endl;
+
+	system("pause");
+	return 0;
+}
